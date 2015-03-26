67e6ae64cc0e99959eadc1eea299e1d8ed629755
staticor <staticor0828@gmail.com>
2015-03-26 18:12:43 +0800

    c

--- ch6-n/6-1.py
+++ ch6-n/6-1.py
@@ -6,27 +6,28 @@
 
 import csv
 
-'testdata =   ./test.csv'
+# 'testdata =   ./test.csv'
 
 
-'normal csv reader'
-with open('test.csv') as f:
-    f_csv = csv.reader(f)
-    header = next(f_csv) # delimiter is ',' can change.
-    print(header); print(type(header))
+with open('d:/datadict.csv', encoding='utf-8') as f:
+    f_csv = csv.DictReader(f)
+    i = 1
     for row in f_csv:
-        pass# print(row)
+        i += 1
+        print(i, )
+        print(row['SYSTEM_TYPE'])
+        print(row['REMARK'])
 
 
-'_ use DictReader of csv'
-with open('test.csv') as f:
-    f_d_csv = csv.DictReader(f)
-    for row in f_d_csv:
-        print(row)
+# '_ use DictReader of csv'
+# with open('test.csv') as f:
+#     f_d_csv = csv.DictReader(f)
+#     for row in f_d_csv:
+#         print(row)
 
 
 
-'some special header'
-header = 'Street Address,Num-Premises,Latitude,Longitude'
-import re
-print(re.sub('[^a-zA-Z_]', ',', header))
+# 'some special header'
+# header = 'Street Address,Num-Premises,Latitude,Longitude'
+# import re
+# print(re.sub('[^a-zA-Z_]', ',', header))
