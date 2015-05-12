7a9a2414890fee1b1e7e27cc435726ca900a3215
staticor <staticor0828@gmail.com>
2015-05-12 17:31:40 +0800

    ch4 add content of Node. deptfirst.

--- ch4/ch4.py
+++ ch4/ch4.py
@@ -30,3 +30,39 @@ class Node:
 		self.val = val
 		self.left = None
 		self.right = None
+
+
+
+class Node(object):
+    def __init__(self, value):
+        self._value = value
+        self._children = []
+
+    def __repr__(self):
+        return 'Node({!r}'.format(self._value)
+
+    def add_child(self, node):
+        self._children.append(node)
+
+    def __iter__(self):
+        return iter(self._children)
+
+    def dept_first(self):
+        yield self
+        for c in self:
+            yield from c.dept_first()
+
+
+root = Node('root')
+child1 = Node('child1')
+child2 = Node('child2')
+
+root.add_child(child1)
+root.add_child(child2)
+
+child1.add_child(Node('child3'))
+child1.add_child(Node('child4'))
+child2.add_child(Node('child5'))
+
+for ch in root.depth_first():
+    print ch
